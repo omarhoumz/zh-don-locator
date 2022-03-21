72a16254e3223a1c6a6b23f5acbbf78c5b79d526
Omar Houmz <omarhoumz@gmail.com>
2022-03-21 23:23:40 +0100

    feat: add user location

--- src/screens/map.tsx
+++ src/screens/map.tsx
@@ -1,5 +1,8 @@
 import { GoogleMap, Marker, useLoadScript } from '@react-google-maps/api'
-import React, { useMemo } from 'react'
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+
+type LatLngLiteral = google.maps.LatLngLiteral
+type MapOptions = google.maps.MapOptions
 
 const position = {
   lat: 33.5577451,
@@ -14,13 +17,30 @@ const positions = [
 ]
 
 export default function Map() {
-  const options = useMemo(
+  const mapRef = useRef<GoogleMap>()
+  const [zoom, setZoom] = useState(12)
+  const [userGeoLocation, setUserGeoLocation] = useState<LatLngLiteral | null>(
+    null,
+  )
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((d) => {
+      const coords = { lat: d.coords.latitude, lng: d.coords.longitude }
+      setUserGeoLocation(coords)
+      setZoom(14)
+      mapRef.current?.panTo(coords)
+    }, console.error)
+  }, [])
+
+  const options = useMemo<MapOptions>(
     () => ({ disableDefaultUI: true, clickableIcons: false }),
     [],
   )
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
   })
+
+  const onLoad = useCallback((map) => (mapRef.current = map), [])
 
   if (!isLoaded) {
     return <div>Loading...</div>
@@ -29,13 +49,21 @@ export default function Map() {
   return (
     <GoogleMap
       center={center}
-      zoom={12}
+      zoom={zoom}
       mapContainerStyle={containerStyle}
       options={options}
+      onLoad={onLoad}
     >
       {positions.map(({ position, title }, index) => {
         return <Marker position={position} title={title} key={index} />
       })}
+
+      {!userGeoLocation ? null : (
+        <Marker
+          position={userGeoLocation}
+          icon="https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
+        />
+      )}
     </GoogleMap>
   )
 }
@@ -45,6 +73,6 @@ const containerStyle = {
   inset: 0,
 }
 const center = {
-  lat: 33.580114,
+  lat: 33.590114,
   lng: -7.5927022,
 }
