3f483f1e1ea88956fe82947afa033a9eb66b0f38
Omar Houmz <omarhoumz@gmail.com>
2022-03-24 22:46:32 +0100

    feat: locate me & go to stores

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,20 +15,36 @@ const Home: NextPage = () => {
     null,
   )
 
-  const tt = useCallback(() => {
-    navigator.geolocation.getCurrentPosition(
-      (d) => {
-        alert('d' + JSON.stringify(d))
-        const coords = { lat: d.coords.latitude, lng: d.coords.longitude }
-        setUserGeoLocation(coords)
-        setZoom(14)
-        mapRef.current?.panTo(coords)
-      },
-      (e) => {
-        alert(e.message)
-      },
-    )
+  const goTo = useCallback((position: LatLngLiteral) => {
+    setZoom(14)
+    mapRef.current?.panTo(position)
   }, [])
+
+  const locateMe = useCallback(() => {
+    if (navigator.geolocation) {
+      if (userGeoLocation) {
+        goTo(userGeoLocation)
+      }
+
+      navigator.geolocation.getCurrentPosition(
+        (d) => {
+          const coords = { lat: d.coords.latitude, lng: d.coords.longitude }
+          if (
+            !userGeoLocation ||
+            coords.lat !== userGeoLocation.lat ||
+            coords.lng !== userGeoLocation.lng
+          ) {
+            setUserGeoLocation(coords)
+            goTo(coords)
+          }
+        },
+        (e) => {
+          const errorMessage = e.message
+          alert(errorMessage + '\n' + 'Please allow location access')
+        },
+      )
+    }
+  }, [goTo, userGeoLocation])
 
   const onLoad = useCallback((d) => (mapRef.current = d), [])
 
@@ -40,7 +56,7 @@ const Home: NextPage = () => {
         <Header />
         <div className='grid flex-grow grid-rows-homepage md:grid-flow-col md:grid-cols-homepage md:grid-rows-1'>
           <div className='order-2 overflow-auto p-4 md:order-1'>
-            <StoresList locateMe={tt} />
+            <StoresList locateMe={locateMe} goTo={goTo} />
           </div>
           <div className='order-1 flex md:order-2'>
             <Map
--- src/screens/stores-list.tsx
+++ src/screens/stores-list.tsx
@@ -1,19 +1,48 @@
 import React from 'react'
 import { stores } from 'src/config/data'
+import type { LatLngLiteral } from './map'
 
-export default function StoresList({ locateMe }: { locateMe: () => void }) {
+type StoreListProps = {
+  locateMe: () => void
+  goTo: (position: LatLngLiteral) => void
+}
+
+export default function StoresList({ locateMe, goTo }: StoreListProps) {
   return (
     <section className='flex flex-col gap-2'>
       <header className='flex items-baseline justify-between'>
-        <h2>33 Superettes</h2>
+        <h2>{stores.length} Superettes</h2>
 
-        <button onClick={locateMe} type='button'>
-          Me Localiser
+        <button
+          onClick={locateMe}
+          type='button'
+          className='flex items-center gap-2 rounded border border-current bg-teal-50 px-3 py-1 text-teal-700 hover:bg-teal-100 hover:text-current'
+        >
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='-ml-1 h-5 w-5'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke='currentColor'
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z'
+            />
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M15 11a3 3 0 11-6 0 3 3 0 016 0z'
+            />
+          </svg>
+          <span>Me Localiser</span>
         </button>
       </header>
 
       <ul>
-        {stores.map(({ title, description }, index) => {
+        {stores.map(({ title, description, position }, index) => {
           return (
             <li
               className='flex items-center justify-between gap-3 border-b py-3'
@@ -23,8 +52,26 @@ export default function StoresList({ locateMe }: { locateMe: () => void }) {
                 <h4 className='font-semibold'>{title}</h4>
                 <p className='font-light'>{description}</p>
               </div>
-              <button className='rounded border border-slate-300 bg-slate-50 px-3 py-1 text-slate-600 ring-2 ring-transparent ring-offset-2 ring-offset-white hover:bg-slate-100 hover:text-slate-800 focus-visible:outline-none focus-visible:ring-slate-400 active:ring-slate-500'>
-                Aller
+              <button
+                className='flex items-center gap-2 rounded border border-slate-300 bg-slate-50 px-3 py-1 text-slate-600 ring-2 ring-transparent ring-offset-2 ring-offset-white hover:bg-slate-100 hover:text-slate-800 focus-visible:outline-none focus-visible:ring-slate-400 active:ring-slate-500'
+                onClick={() => goTo(position)}
+              >
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  className='-ml-1 h-4 w-4'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  stroke='currentColor'
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    d='M13 5l7 7-7 7M5 5l7 7-7 7'
+                  />
+                </svg>
+
+                <span>Aller</span>
               </button>
             </li>
           )
